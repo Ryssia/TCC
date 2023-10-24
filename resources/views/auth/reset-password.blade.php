300b7bb3cd747a232ae7fce2172e196a987b040c
Ryssia.Elias <68912012+Ryssia@users.noreply.github.com>
2023-10-24 11:00:55 -0300

    view reset/forgot password
    falta configurar o MAIL_ENCRYPTION; arrumar cards, tirar fundo rosa, ajeitar as views

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
+            <a href="reset-password">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
@@ -9,39 +9,45 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.update') }}">
+        <form method="POST" action="{{ route('reset-password') }}">
             @csrf
 
             <!-- Password Reset Token -->
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <div class="form-floating mb-3" style="width: 80%;">
+                        <x-label for="email" :value="__('Email')" />
+                        <x-input id="email" class="form-control" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                    </div>
+                </div>
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <div class="form-floating mb-3" style="width: 80%;">
+                        <x-label for="password" :value="__('Password')" />
+                        <x-input id="password" class="form-control" type="password" name="password" required />
+                    </div>
+                </div>
             </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <div class="form-floating mb-3" style="width: 80%;">
+                        <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <x-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required />
+                    </div>
+                </div>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
+            <div class="row mt-2">
+                <div class="col d-flex justify-content-center">
+                    <x-button>
+                        {{ __('Reset Password') }}
+                    </x-button>
+                </div>
             </div>
         </form>
     </x-auth-card>
