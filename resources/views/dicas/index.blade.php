1bddefd179f2dff07559bd6c2eb5c8a3fbdbefd9
Ryssia.Elias <68912012+Ryssia@users.noreply.github.com>
2023-11-01 15:33:39 -0300

    cards arrumados

--- resources/views/dicas/index.blade.php
+++ resources/views/dicas/index.blade.php
@@ -4,13 +4,12 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="{{asset('/css/card.css')}}" />
 
-<div class="row ">
-
-    <div class="categoria d-flex justify-content-center align-items-center">
-        <div class="col col-md-6 offset-md-3 text-center"> 
-          
+<div class="container">
+    <div class="row ">    
+        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
           @foreach($dica_fluxo as $dica)  
-              <div class="card">
+              
+              <div class="card col-lg-4 col-12 ">
                       <div class="additional fluxo">
                         <div class="left-card">
                           <!-- categoria -->
@@ -22,16 +21,18 @@
                         <!-- descricao -->
                         <p>{{ $dica->descricao }}</p>
                       </div>
-                    </div>
-              </div>
+              </div>                     
           @endforeach
-
+        </div>    
+        
+        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
           @foreach($dica_colica as $dica)  
-              <div class="card">
+              
+              <div class="card col-lg-4 col-12 ">
                       <div class="additional colica">
                         <div class="left-card">
                           <!-- categoria -->
-                          <div class="category center">Cólica</div> 
+                          <div class="category center">Colica</div> 
                           <i class="bi bi-lightning-fill center icon"></i>
                         </div>
                       </div>
@@ -39,16 +40,18 @@
                         <!-- descricao -->
                         <p>{{ $dica->descricao }}</p>
                       </div>
-                    </div>
-              </div>
+              </div>                     
           @endforeach
+        </div>    
 
+        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
           @foreach($dica_absorvente as $dica)  
-              <div class="card">
+              
+              <div class="card col-lg-4 col-12 ">
                       <div class="additional absorvente">
                         <div class="left-card">
                           <!-- categoria -->
-                          <div class="category center">Absorventes</div> 
+                          <div class="category center">Absorvente</div> 
                           <i class="bi bi-postage-heart center icon"></i>
                         </div>
                       </div>
@@ -56,10 +59,9 @@
                         <!-- descricao -->
                         <p>{{ $dica->descricao }}</p>
                       </div>
-                    </div>
-              </div>
+              </div>                     
           @endforeach
-        </div>
+        </div>    
     </div>
 </div>
 
