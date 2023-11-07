a0094d47b8369bcf3ad6fe665cd9583f3b32cd20
Ryssia <ryssia.elias@gmail.com>
2023-11-07 17:21:38 -0300

    ajustes dos cards e banco de dados de dicas

--- app/Http/Controllers/DicaController.php
+++ app/Http/Controllers/DicaController.php
@@ -10,10 +10,10 @@ class DicaController extends Controller
     public function index()
     {
         //$dicas = Dica::all();
-        $dica_absorvente = Dica::where('categoria_id', 1)->get();
-        $dica_fluxo = Dica::where('categoria_id', 2)->get();
-        $dica_colica = Dica::where('categoria_id', 3)->get();
-        $dica_geral = Dica::where('categoria_id', 4)->get();
-        return view('dicas.index', compact('dica_absorvente', 'dica_fluxo', 'dica_colica','dica_geral')); 
+        $dica_fluxo = Dica::where('categoria_id', 1)->get();
+        $dica_colica = Dica::where('categoria_id', 2)->get();
+        $dica_dores = Dica::whereIn('categoria_id',[3, 4])->get(); //pega tanto dor de cabeça, quanto dor nos seios
+        $dica_absorvente = Dica::where('categoria_id', 5)->get();
+        return view('dicas.index', compact('dica_fluxo', 'dica_colica', 'dica_dores', 'dica_absorvente')); 
     }
 }
--- resources/views/dicas/index.blade.php
+++ resources/views/dicas/index.blade.php
@@ -5,64 +5,91 @@
 <link rel="stylesheet" href="{{asset('/css/card.css')}}" />
 
 <div class="container">
-    <div class="row ">    
-        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
-          @foreach($dica_fluxo as $dica)  
-              
-              <div class="card col-lg-4 col-12 ">
-                      <div class="additional fluxo">
-                        <div class="left-card">
-                          <!-- categoria -->
-                          <div class="category center">Fluxo</div> 
-                          <i class="bi bi-droplet-half center icon"></i>
+      <div class="row ">    
+        <!-- CARD DE FLUXO -->
+          <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
+            @foreach($dica_fluxo as $dica)  
+              @if($dica->notificacao == 0) 
+                <div class="card col-lg-4 col-12 ">
+                        <div class="additional fluxo">
+                          <div class="left-card">
+                            <!-- categoria -->
+                            <div class="category center">Fluxo</div> 
+                            <i class="bi bi-droplet-half center icon"></i>
+                          </div>
                         </div>
-                      </div>
-                      <div class="general d-flex justify-content-center align-items-center">
-                        <!-- descricao -->
-                        <p>{{ $dica->descricao }}</p>
-                      </div>
-              </div>                     
-          @endforeach
-        </div>    
-        
-        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
-          @foreach($dica_colica as $dica)  
-              
-              <div class="card col-lg-4 col-12 ">
-                      <div class="additional colica">
-                        <div class="left-card">
-                          <!-- categoria -->
-                          <div class="category center">Colica</div> 
-                          <i class="bi bi-lightning-fill center icon"></i>
+                        <div class="general d-flex justify-content-center align-items-center">
+                          <!-- descricao -->
+                          <p>{{ $dica->descricao }}</p>
                         </div>
-                      </div>
-                      <div class="general d-flex justify-content-center align-items-center">
-                        <!-- descricao -->
-                        <p>{{ $dica->descricao }}</p>
-                      </div>
-              </div>                     
-          @endforeach
-        </div>    
+                </div>    
+              @endif                 
+            @endforeach
+          </div>    
+          
+          <!-- CARD DE CÓLICA -->
+          <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
+            @foreach($dica_colica as $dica)  
+              @if($dica->notificacao == 0)
+                <div class="card col-lg-4 col-12 ">
+                        <div class="additional colica">
+                          <div class="left-card">
+                            <!-- categoria -->
+                            <div class="category center">Colica</div> 
+                            <i class="bi bi-lightning-fill center icon"></i>
+                          </div>
+                        </div>
+                        <div class="general d-flex justify-content-center align-items-center">
+                          <!-- descricao -->
+                          <p>{{ $dica->descricao }}</p>
+                        </div>
+                </div> 
+               @endif                      
+            @endforeach
+          </div>    
 
-        <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
-          @foreach($dica_absorvente as $dica)  
-              
-              <div class="card col-lg-4 col-12 ">
-                      <div class="additional absorvente">
-                        <div class="left-card">
-                          <!-- categoria -->
-                          <div class="category center">Absorvente</div> 
-                          <i class="bi bi-postage-heart center icon"></i>
+          <!-- CARD DE ABSORVENTE -->
+          <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
+            @foreach($dica_absorvente as $dica)  
+              @if($dica->notificacao == 0)  
+                <div class="card col-lg-4 col-12 ">
+                        <div class="additional absorvente">
+                          <div class="left-card">
+                            <!-- categoria -->
+                            <div class="category center">Absorvente</div> 
+                            <i class="bi bi-postage-heart center icon"></i>
+                          </div>
                         </div>
-                      </div>
-                      <div class="general d-flex justify-content-center align-items-center">
-                        <!-- descricao -->
-                        <p>{{ $dica->descricao }}</p>
-                      </div>
-              </div>                     
-          @endforeach
-        </div>    
-    </div>
+                        <div class="general d-flex justify-content-center align-items-center">
+                          <!-- descricao -->
+                          <p>{{ $dica->descricao }}</p>
+                        </div>
+                </div> 
+              @endif                    
+            @endforeach
+          </div>  
+          
+          <!-- CARD DE DORES -->
+          <div class="categoria d-flex flex-column flex-lg-row flex-wrap justify-content-center align-items-center"> 
+            @foreach($dica_absorvente as $dica)  
+              @if($dica->notificacao == 0)  
+                <div class="card col-lg-4 col-12 ">
+                        <div class="additional absorvente">
+                          <div class="left-card">
+                            <!-- categoria -->
+                            <div class="category center">Absorvente</div> 
+                            <i class="bi bi-postage-heart center icon"></i>
+                          </div>
+                        </div>
+                        <div class="general d-flex justify-content-center align-items-center">
+                          <!-- descricao -->
+                          <p>{{ $dica->descricao }}</p>
+                        </div>
+                </div> 
+              @endif                    
+            @endforeach
+          </div>
+      </div>
 </div>
 
 @endsection
