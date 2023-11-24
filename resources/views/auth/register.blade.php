3ec51edd71762b59f242998b01e3498b8a6b019e
Ryssia <ryssia.elias@gmail.com>
2023-11-24 18:44:34 -0300

    tentando corrigir register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -110,7 +110,7 @@
                         </div>
                         <div class="row mt-2">
                             <div class="col">
-                                <button type="submit" class="btn text-white" style="background-color: #f38ebd; width: 80%;">
+                                <button href="{{route('home')}}" type="submit" class="btn text-white" style="background-color: #f38ebd; width: 80%;">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
                                         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
@@ -126,13 +126,13 @@
                         <div class="col-lg-6 col-md-12 mt-1">
                         </div>
                         <div class="col-lg-6 col-md-12 mt-1">
-                            <a href="/" class="btn text-decoration-none text-white" style="background-color: #f3b3d1; width: 70%;">
+                            <button class="btn text-white" style="background-color: #9fa1f8; width: 90%;">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"/>
                                     <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                                 </svg>
                                 &nbsp;Já sou cadastrada
-                            </a>    
+                            </button>    
                         </div>
                     </div>
                 </div>
