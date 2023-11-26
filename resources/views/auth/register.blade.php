94814e2e568050002e2ad374c4c449297f088450
Ryssia.Elias <ryssia.elias@gmail.com>
2023-11-26 11:32:59 -0300

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -110,7 +110,7 @@
                         </div>
                         <div class="row mt-2">
                             <div class="col">
-                                <button href="{{route('home')}}" type="submit" class="btn text-white" style="background-color: #f38ebd; width: 80%;">
+                                <button type="submit" class="btn text-white" style="background-color: #f38ebd; width: 80%;">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
                                         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
